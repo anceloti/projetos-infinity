287967ef454e0fe0beab76c6e1306dee33fa4f14
Joao <jgperfect14@gmail.com>
2024-10-25 00:02:07 -0300

    prova1

---
+++ PROVA1.py
@@ -0,0 +1,9 @@
+# 1
+# [LPIA-A01] Você está criando um programa para calcular a área de um retângulo.
+#  O programa deve solicitar ao usuário que forneça a base e a altura do retângulo. 
+# Em seguida, o programa deve calcular a área usando a fórmula:
+# Área=Base×Altura
+
+base = int(input('Forneça o valor da base do retângulo: '))
+altura = int(input('Forneça o valor da altura do retângulo: '))
+print ("o valor da àrea do retângulo é:", base * altura)
